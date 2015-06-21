326b30815687ccc35ac6c12de1764fe230effab8
Brian Bosak <webadm@elcnet.servehttp.com>
2015-06-21 18:21:36 -0500

    Misc changes

--- GC.cpp
+++ GC.cpp
@@ -39,6 +39,20 @@ static inline void MEM_RemovePtr(void* region, void* ptr) {
   meta_start[2]--;
 }
 
+static inline size_t MEM_DataSize(void* region) {
+  size_t* ptr = (size_t*)region;
+  return ptr[1]-ptr;
+}
+
+static inline size_t MEM_MovePtr(void* dest, void* src) {
+  size_t* ptr = (size_t*)src;
+  for(size_t i = 0;i<ptr[2];i++) {
+    void** d = ptr[i+3];
+    *d = dest;
+  }
+}
+
+
 class GCGeneration {
 public:
   unsigned char* memory_unaligned;
@@ -60,6 +74,7 @@ public:
     freeList_size = 0;
     freeList_capacity = 0;
     freeList = 0;
+    next = 0;
   }
   void Free(void* ptr) {
     //Add object to free list
@@ -83,15 +98,22 @@ public:
   }
   //Write barrier, mark
   void WB_Mark(void*& ptr) {
+    size_t* realPtr = (size_t*)ptr;
     if(MEM_ListCapacity(ptr)-MEM_ListLength(ptr) == 0) {
       //Expand list
-      size_t* realPtr = (size_t*)ptr;
       size_t prevCapacity = MEM_ListCapacity(ptr);
       size_t newCapacity = prevCapacity*2;
       //Allocate new block
-      Unsafe_Allocate();
+      void* newmem = Unsafe_Allocate((3*sizeof(size_t))+newCapacity+MEM_DataSize(ptr));
+      MEM_MovePtr(newmem,ptr);
+      memcpy(newmem,ptr,realPtr[0]);
+      ptr = newmem;
     }
+    MEM_AddPtr(realPtr,&ptr);
     
+  }
+  void WB_Unmark(void*& ptr) {
+    MEM_RemovePtr(ptr,&ptr);
   }
   void* Unsafe_Allocate(size_t sz) {
     if(sz>Available()) {
@@ -114,31 +136,56 @@ public:
       }
       return 0;
   }
+  GCGeneration* next;
+  bool Contains(void* ptr) {
+    size_t c = (size_t)ptr;
+    size_t a = memory;
+    size_t b = memory+marker;
+    return c>=a && c<=b;
+  }
   ~GCGeneration() {
     delete[] memory_unaligned;
     if(freeList) {
       delete[] freeList;
     }
+    if(next) {
+      delete next;
+    }
   }
 };
 class GCPool {
 public:
-  GC_Ptr* ptrTable;
-  size_t ptrTable_sz;
-  GCPool() {
-    ptrTable_sz = 0;
-    ptrTable = new GC_Ptr[80];
-    ptrTable_sz = 80;
-  }
-  GC_Ptr* allocptr() {
-    
+  //The VERY FIRST generation of Star Trek!
+  GCGeneration* firstGeneration;
+  GCPool(size_t generations) {
+    firstGeneration = new GCGeneration();
   }
   ~GCPool() {
-    delete[] ptrTable;
+    delete firstGeneration;
   }
 };
-
-static GCPool pool;
 extern "C" {
-  
+  void* GC_Init(size_t generations) {
+    return new GCPool();
+  }
+  void GC_Allocate(void* gc,size_t sz, void** output) {
+    GCGeneration* gen = ((GCPool*)gc)->firstGeneration;
+    void* ptr = gen->Allocate(sz);
+    *output = ptr;
+    gen->WB_Mark(*output);
+  }
+  void GC_Unmark(void* gc,void** ptr) {
+    GCGeneration* gen = ((GCPool*)gc)->firstGeneration;
+    while(!gen->Contains(*ptr)) {
+      gen = gen->next;
+    }
+    gen->WB_Unmark(*ptr);
+  }
+  void GC_Mark(void* gc,void** ptr) {
+    GCGeneration* gen = ((GCPool*)gc)->firstGeneration;
+    while(!gen->Contains(*ptr)) {
+      gen = gen->next;
+    }
+    gen->WB_Mark(*ptr);
+  }
 }
